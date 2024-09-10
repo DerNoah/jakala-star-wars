212d8926583f30903e0d9f7cdd8d513126dce904
Noah <noah.pluetzer+git@gmail.com>
2024-09-10 11:33:49 +0200

    add functionality to push and pop in preview coordinator

--- jakala-star-wars-project/Common/PreviewCoordinator/PreviewCoordinator.swift
+++ jakala-star-wars-project/Common/PreviewCoordinator/PreviewCoordinator.swift
@@ -7,10 +7,28 @@
 
 import Foundation
 import SUICoordinator
+import SwiftUI
 
 final class PreviewCoordinator: Coordinator {
-    func push<D>(destination: D) where D: SUICoordinator.Destination {}
-    func pop() {}
+    @Published var navigationPath: NavigationPath
+    
+    public init(navigationPath: NavigationPath) {
+        self.navigationPath = navigationPath
+    }
+    
+    public func push<D>(destination: D) where D: Destination {
+        let anyDestination = AnyDestination(
+            identifier: destination.identifier,
+            viewBuilder: { [unowned self] dependencyContainer in
+                destination.makeView(with: dependencyContainer, coordinator: self)
+            }
+        )
+        navigationPath.append(anyDestination)
+    }
+    
+    public func pop() {
+        navigationPath.removeLast()
+    }
 }
 
 extension PreviewCoordinator: DependencyIdentifier {
