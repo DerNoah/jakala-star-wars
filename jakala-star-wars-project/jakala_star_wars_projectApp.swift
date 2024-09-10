93edd59afd945c9c5ef326106ccf22d11afa3c20
Noah <noah.pluetzer+git@gmail.com>
2024-09-10 10:46:30 +0200

    initial commit

---
+++ jakala-star-wars-project/jakala_star_wars_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  jakala_star_wars_projectApp.swift
+//  jakala-star-wars-project
+//
+//  Created by Noah Plützer on 10.09.24.
+//
+
+import SwiftUI
+
+@main
+struct jakala_star_wars_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
